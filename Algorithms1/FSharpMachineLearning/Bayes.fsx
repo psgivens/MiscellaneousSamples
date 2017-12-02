4d4b26136c9ab93a3f1a33b064db38af6389df41
psgivens <psgivens11@yahoo.com>
2017-12-01 17:47:14 -0800

    Playing with some Machine Learning in F#

---
+++ Algorithms1/FSharpMachineLearning/Bayes.fsx
@@ -0,0 +1,112 @@
+﻿open System.IO
+
+type DocType =
+    | Ham
+    | Spam
+
+let parseDocType = function
+    | "ham" -> Ham
+    | "spam" -> Spam 
+    | _ -> failwith "Unknown label"
+
+let parseLine (line:string) =
+    let split = line.Split '\t'
+    let label = split.[0] |> parseDocType
+    let message = split.[1]
+    (label, message)
+
+let fileName = "SMSSpamCollection"
+let path = __SOURCE_DIRECTORY__ + @"\Data\" + fileName
+
+let dataset = 
+    File.ReadAllLines path
+    |> Array.map parseLine
+
+let spamWithFree = 
+    dataset 
+    |> Array.filter (fun (docType,_) -> docType = Spam)
+    |> Array.filter (fun (_,sms) -> sms.Contains "FREE")
+    |> Array.length
+
+let hamWithFREE = 
+    dataset 
+    |> Array.filter (fun (docType,_) -> docType = Ham)
+    |> Array.filter (fun (_,sms) -> sms.Contains "FREE")
+    |> Array.length
+
+let primitiveClassifier (sms:string) =
+    if (sms.Contains "FREE") 
+    then Spam
+    else Ham
+
+dataset |> Seq.length
+
+#load "NaiveBayes.fsx"
+open NaiveBayes.Classifier
+
+tokenizeWords "42 is the Answer to the question";;
+
+let training = 
+    dataset 
+    |> Seq.skip 1000
+    |> Seq.toArray
+
+let validation =
+    dataset
+    |> Seq.take 1000
+    |> Seq.toArray
+
+let txtClassifier = train training tokenizeWords (["txt"] |> set)
+txtClassifier "text"
+
+// 
+validation 
+|> Seq.mapi (fun i (d,s) -> i,d,s)
+|> Seq.averageBy (fun (i, docType, sms) ->
+//    printfn "%4d procesed %A from %s" i docType sms
+    if docType = txtClassifier sms 
+    then 1.0 
+    else 0.0)
+|> printfn "Based on 'txt', correctly classified: %.3f"
+
+// Baseline
+validation 
+|> Seq.mapi (fun i (d,s) -> i,d,s)
+|> Seq.averageBy (fun (i, docType, sms) ->
+//    printfn "%4d procesed %A from %s" i docType sms
+    if docType = Ham
+    then 1.0 
+    else 0.0)
+|> printfn "Assuming everything is Ham, correctly classified: %.3f"
+
+let vocabulary (tokenize:Tokenizer) (corpus:string seq) =
+    corpus 
+    |> Seq.map tokenize
+    |> Set.unionMany
+
+let allTokens =
+    training
+    |> Seq.map snd
+    |> vocabulary tokenizeWords
+
+let classifyWithAll = train training tokenizeWords allTokens
+txtClassifier "text me when you are here"
+
+// 
+validation 
+|> Seq.mapi (fun i (d,s) -> i,d,s)
+|> Seq.averageBy (fun (i, docType, sms) ->
+//    printfn "%4d procesed %A from %s" i docType sms
+    if docType = classifyWithAll sms 
+    then 1.0 
+    else 0.0)
+|> printfn "Based on 'txt', correctly classified: %.3f"
+
+
+
+
+
+
+
+
+
---
+++ Algorithms1/FSharpMachineLearning/NaiveBayes.fsx
@@ -0,0 +1,81 @@
+﻿namespace NaiveBayes
+
+module Classifier =
+
+    type Token = string
+    type TokenizedDoc = Token Set
+    type Tokenizer = string -> TokenizedDoc
+
+    type ClassificationGroup = {   
+        Proportion:float
+        TokenFrequencies:Map<Token,float> }
+
+    let tokenScore (classification:ClassificationGroup) token =
+        match classification.TokenFrequencies.TryFind token with
+        | Some(value) -> log value
+        | None -> 0.0
+
+    let score document (classification:ClassificationGroup) =
+        let scoreToken = tokenScore classification
+        log classification.Proportion + (document |> Seq.sumBy scoreToken)
+
+    let classify 
+            (groups:(_*ClassificationGroup) seq) 
+            (tokenize:Tokenizer)
+            (txt:string) =
+        let document = tokenize txt
+        groups
+        |> Seq.maxBy (fun (_, group) -> score document group)
+        |> fst
+
+    let proportion count total = float count / float total
+    let laplace count total = float (count + 1) / float (total + 1)
+    let countContaining token (documents:TokenizedDoc seq) =
+        documents 
+        |> Seq.filter (Set.contains token)
+        |> Seq.length
+                
+    let analyze 
+            (docs:TokenizedDoc seq)
+            (totalDocs:int)
+            (classificationTokens:Token Set) =
+        let docsCount = docs |> Seq.length
+        let score token =
+            let count = docs |> countContaining token
+            laplace count docsCount
+        let scoredTokens =
+            classificationTokens
+            |> Set.map (fun token -> token, score token)
+            |> Map.ofSeq
+        let groupProportion = proportion docsCount totalDocs
+        {   Proportion = groupProportion
+            TokenFrequencies = scoredTokens }
+
+    let learn 
+            (docs:(_*string)[])
+            (tokenize:Tokenizer)
+            (classificationTokens:Token Set) =
+        let total = docs.Length
+        docs 
+        |> Seq.groupBy fst
+        |> Seq.map (fun (label, docGroup) -> label, docGroup |> Seq.map (snd >> tokenize))
+        |> Seq.map (fun (label, groupDocs) -> label, analyze groupDocs total classificationTokens)
+        |> Seq.toList
+
+    let train 
+            (docs:(_*string)[])
+            (tokenize:Tokenizer)
+            (classificationTokens:Token Set) =
+        let groups = learn docs tokenize classificationTokens
+        classify groups tokenize
+        
+    open System.Text.RegularExpressions
+    let matchWords = Regex @"\w+"
+    let tokenizeWords (text:string) = 
+        text.ToLowerInvariant ()
+        |> matchWords.Matches
+        |> Seq.cast<Match>
+        |> Seq.map (fun m -> m.Value)
+        |> Set.ofSeq
+
+        
