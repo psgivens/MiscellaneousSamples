52dabcfed7a7af1742a8f5c0c2350fe246678cfc
psgivens <psgivens11@yahoo.com>
2017-12-23 10:02:36 -0800

    Moved ToastmastersRecord solution to https://github.com/psgivens/ToastmastersRecord

--- ToastmastersRecords/ToastmastersRecord.Actors/Infrastructure.fs
+++
@@ -1,7 +0,0 @@
-﻿module ToastmastersRecord.Actors.Infrastructure
-
-open Akka.Actor
-open Akka.FSharp
-open ToastmastersRecord.Domain.Infrastructure
-
-type ActorIO<'a> = { Tell:Envelope<'a> -> unit; Actor:IActorRef; Events:IActorRef; Errors:IActorRef }
