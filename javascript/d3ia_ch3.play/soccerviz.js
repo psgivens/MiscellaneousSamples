cae525b3fa7f620c34d0df63f23c03b2901fc7c3
psgivens <psgivens11@yahoo.com>
2017-12-24 09:59:19 -0800

    Working on a Horizontal Grouped Bar Graph.

--- javascript/d3ia_ch3.play/soccerviz.js
+++ javascript/d3ia_ch3.play/soccerviz.js
@@ -1,20 +1,71 @@
 function createSoccerViz() {
-  d3.csv("worldcup.csv", function(data) {
+  // d3.csv("worldcup.csv", function(data) {
+  //   overallTeamViz(data);
+  // })
+  d3.csv("defects.csv", function(data) {
     overallTeamViz(data);
   })
 
   function overallTeamViz(incomingData) {
 
-    const xScale = d3.scaleLinear()
-      .domain([ 0, 500 ])
-      .range([ 0, 500 ]);
+    const margin = {top: 20, right: 80, bottom: 50, left: 80};
+    const width = 600 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+    const catSize = height / 3;
+    const labelOffset = catSize / 2 + margin.top;
+    const tickSize = 10;
 
-    const xaxis = d3.axisTop().scale(xScale).tickSize(20).ticks(4);
+    const categories = ["High", "Medium", "Low"]
+
+    const dataByState = d3.nest()
+      .key(d => d.state)
+      .entries(incomingData);
+
+    // const yScale = d3.scaleBand()
+    //   .domain(ndata.map(d => d.key))
+    //   .range([0, 440])
+    const yScale = d3.scaleBand()
+      .domain(categories)
+      .range([0, 440])
+
+
+    const yaxis = d3.axisLeft()
+      .scale(yScale)
+      .tickSize(tickSize)
+      .ticks(4);
+
     d3.selectAll("svg")
       .append("g")
-      .attr("id","xAxisG")
-      .call(xaxis)
-      .attr("transform","translate(0,40)");
+      .attr("id","yAxisG")
+      .call(yaxis)
+      .attr("transform","translate(70,10)");
+
+    const xScale = d3.scaleLinear()
+      .domain([ 0, 500 ])
+      .range([ 0, 180 ]);
+
+    const xaxis = d3.axisTop()
+      .scale(xScale)
+      .tickSize(tickSize)
+      .ticks(4);
+
+    dataByState.map(function(d, i) {
+      d3.selectAll("svg")
+        .append("g")
+        .attr("id","xAxisG1" + d.key)
+        .call(xaxis)
+        .attr("transform","translate("+ (margin.left + i * 200) +",495)");
+
+      console.log("==========================");
+      console.log("Graphing: " + d.key);
+      console.log(d.values);
+      var tempData = d.values.reduce((acc,o) => {acc[o.severity] = o; return acc}, {});
+
+      for (severity of categories) {
+        console.log("Severity: " + severity + "  count: " + (tempData[severity] != null ?  tempData[severity].count : 0));
+      }
+      // d.map(kvp => console.log(kvp.key));
+    });
 
     const teamG = d3.select("svg")
       // create graphics element called teamsG to serve as a container
