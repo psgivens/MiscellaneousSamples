e3a22ed98de3ba6d9ba9a032e6c71ba84fd3dd75
psgivens <psgivens11@yahoo.com>
2017-12-25 09:40:06 -0800

    Finished first round of creating my horizontal grouped bar chart.

--- javascript/d3ia_ch3.play/soccerviz.js
+++ javascript/d3ia_ch3.play/soccerviz.js
@@ -6,11 +6,17 @@ function createSoccerViz() {
     overallTeamViz(data);
   })
 
+  function barId(severity, state){
+      return "bar_" + severity + "_" + state;
+  }
+
   function overallTeamViz(incomingData) {
 
+    const svgHeight = 200;
+    const svgWidth = 900;
     const margin = {top: 20, right: 80, bottom: 50, left: 80};
-    const width = 600 - margin.left - margin.right;
-    const height = 500 - margin.top - margin.bottom;
+    const width = svgWidth - margin.left - margin.right;
+    const height = svgHeight - margin.top - margin.bottom;
     const catSize = height / 3;
     const labelOffset = catSize / 2 + margin.top;
     const tickSize = 10;
@@ -21,13 +27,11 @@ function createSoccerViz() {
       .key(d => d.state)
       .entries(incomingData);
 
-    // const yScale = d3.scaleBand()
-    //   .domain(ndata.map(d => d.key))
-    //   .range([0, 440])
     const yScale = d3.scaleBand()
       .domain(categories)
-      .range([0, 440])
-
+      .range([margin.top, svgHeight - margin.bottom])
+      .padding(0.2)
+      .round(true);
 
     const yaxis = d3.axisLeft()
       .scale(yScale)
@@ -38,114 +42,65 @@ function createSoccerViz() {
       .append("g")
       .attr("id","yAxisG")
       .call(yaxis)
-      .attr("transform","translate(70,10)");
+      .attr("transform","translate(" + margin.left + "," + margin.top + ")");
 
-    const xScale = d3.scaleLinear()
-      .domain([ 0, 500 ])
-      .range([ 0, 180 ]);
-
-    const xaxis = d3.axisTop()
-      .scale(xScale)
-      .tickSize(tickSize)
-      .ticks(4);
+    const xScale_InterState = d3.scaleBand()
+      .domain(["BL","IP", "Done"])
+      .range([ margin.left, width + margin.left ])
+      .padding(0.1)
+      .round(true);
 
     dataByState.map(function(d, i) {
+
+      // TODO: Build xScale per category
+      const xScale_State = d3.scaleLinear()
+        .domain([ 0, 7 ])
+        .range([ 0, 0 + xScale_InterState.bandwidth() ]);
+
+      const xAxis = d3.axisTop()
+        .scale(xScale_State)
+        .tickSize(tickSize)
+        .ticks(4);
+
       d3.selectAll("svg")
         .append("g")
-        .attr("id","xAxisG1" + d.key)
-        .call(xaxis)
-        .attr("transform","translate("+ (margin.left + i * 200) +",495)");
+        .attr("id","xAxis_" + d.key)
+        .call(xAxis)
+        .attr("transform","translate(" + xScale_InterState(d.key) + "," + (svgHeight ) + ")");
 
-      console.log("==========================");
-      console.log("Graphing: " + d.key);
-      console.log(d.values);
       var tempData = d.values.reduce((acc,o) => {acc[o.severity] = o; return acc}, {});
 
-      for (severity of categories) {
-        console.log("Severity: " + severity + "  count: " + (tempData[severity] != null ?  tempData[severity].count : 0));
-      }
-      // d.map(kvp => console.log(kvp.key));
-    });
+      // https://bl.ocks.org/EmbraceLife/677054c8f535c77ddd95485523d97fcd
+      const chart = d3.selectAll("svg").append("g")
+        .attr("id", "graph" + d.key);
 
-    const teamG = d3.select("svg")
-      // create graphics element called teamsG to serve as a container
-      .append("g")
-      .attr("id", "teamsG")
-      .attr("transform", "translate(50,300)")
-      .selectAll("g")
-      .data(incomingData)
-      .enter()
-      .append("g")
-      .attr("class", "overallG")
-      .attr("transform",
-         function (d,i) {return "translate(" + (i * 50) + ", 0)"}
-        );
+      const bar = chart.selectAll("g")
+        .data(d.values)
+        .enter().append("g")
+        .attr("transform", function(d1) { return "translate(" + xScale_InterState(d.key) + ", " + margin.top + ")"; });
 
-      teamG
-        .append("circle")
-        // Style the circle
+      bar.append("rect")
         .style("fill", "pink")
         .style("stroke", "black")
         .style("stroke-width", "1px")
-        // Start with nothing and transition to 40
-        .attr("r", 0)
-        .transition()
-        .delay(function(d,i) {return i * 100})
-        .duration(500)
-        .attr("r", 40)
-        // Transition back to 20
-        .transition()
-        .duration(500)
-        .attr("r", 20);
-
-    teamG
-      .append("text")
-      .style("text-anchor", "middle")
-      .attr("y", 30)
-      .style("font-size", "10px")
-      .text(function(d, i) {return d.team + " " + i;});
-    const dataKeys = d3.keys(incomingData[0]).filter(function(el) {
-       return el != "team" && el != "region";
+        .attr("id", function(d1) { return barId(d1.severity,d1.state);})
+        .attr("class", "measurementRectangle")
+        .attr("y", function(d) { return yScale(d.severity); })
+        .attr("x", function(d) { return 0; })
+        .attr("height", function(d) { return yScale.bandwidth(); })
+        .attr("width", function(d) { return xScale_State(d.count); });
     });
 
-    teamG
-      .on("mouseover", highlightRegion);
+    const mRects = d3.selectAll("rect.measurementRectangle");
+    mRects.on("mouseover", function(d) {
+      const bar = d3.select(this);
+      bar.style("fill","red");
+    });
 
-    function highlightRegion(d) {
-         d3.selectAll("g.overallG")
-           .select("circle")
-           .style("fill", function(p) {
-             return p.region == d.region ? "red" : "gray";
-           });
+    mRects.on("mouseout", function(d) {
+      const bar = d3.select(this);
+      bar.style("fill","pink");
+    });
 
-    };
-
-    d3.select("#controls")
-      .selectAll("button.teams")
-      .data(dataKeys)
-      .enter()
-      .append("button")
-      .classed("team", true)
-      .on("click", buttonClick)
-      .html(function(d) {return d;});
-
-    function buttonClick(datapoint) {
-      const maxValue = d3.max(incomingData, function(d) {
-        return parseFloat(d[datapoint]);
-      });
-
-      const radiusScale = d3.scaleLinear()
-        .domain([ 0, maxValue ])
-        .range([ 2, 20 ]);
-
-      d3.selectAll("g.overallG")
-        .select("circle")
-        .transition()
-        .duration(1000)
-        .attr("r", function(p) {
-              return radiusScale(p[datapoint]);
-        });
-
-    };
   }
 }
