791bfaf9cd85f3a55855021dc02c754233e44790
psgivens <psgivens11@yahoo.com>
2017-12-28 22:49:42 -0800

    Code clean up. Added category labels.

--- javascript/d3ia_ch3.play/barcharts.js
+++ javascript/d3ia_ch3.play/barcharts.js
@@ -1,27 +1,14 @@
 function createBarCharts() {
-  // d3.csv("worldcup.csv", function(data) {
-  //   overallTeamViz(data);
-  // })
   d3.csv("defects.csv", function(data) {
     overallTeamViz(data);
   })
-
-const colors = ["#C0392B", "#E59866", "#FCF3CF"];
-
-  function summarizeData(data) {
-    const severity = d1 => d1.severity;
-    const team = d1 => d1.team;
-    const state = d1 => d1.state;
-    const rollupSum = d1 => d3.sum(d1, d2 =>  d2.count);
-
+  function summarizeData(data, teams) {
     const teamSums = d3.nest()
-      .key(state)
-      .key(team)
-      .key(severity)
-      .rollup(rollupSum)
+      .key(d => d.state)
+      .key(d => d.team)
+      .key(d => d.severity)
+      .rollup(d => d3.sum(d, d1 => d1.count))
       .entries(data);
-
-    const teams = Array.from(data.reduce((acc,d1) => acc.add(d1.team), new Set ()));
 
     const mapTeam = team => {
       const severities = team.values;
@@ -39,18 +26,17 @@ const colors = ["#C0392B", "#E59866", "#FCF3CF"];
     }
 
     const teamSumsR = teamSums
-      .map(stateItem => {
-        return {
+      .map(stateItem => Object.create({
           state:stateItem.key,
           teams:stateItem.values.map(mapTeam)
-        }
-      });
+        }));
 
     return teamSumsR;
   }
 
   function overallTeamViz(incomingData) {
 
+    // Set up sizing constants
     const svgHeight = 200;
     const svgWidth = 900;
     const margin = {top: 20, right: 80, bottom: 50, left: 80};
@@ -59,27 +45,19 @@ const colors = ["#C0392B", "#E59866", "#FCF3CF"];
     const catSize = height / 3;
     const labelOffset = catSize / 2 + margin.top;
     const tickSize = 10;
+    const labelMargin = 30;
+    const labelHeight = 30;
 
-    const dataSums = summarizeData(incomingData);
-    const severities = Array.from(incomingData.reduce((acc,d1) => acc.add(d1.severity), new Set ()));
+    // Munge the data
     const teams = Array.from(incomingData.reduce((acc,d1) => acc.add(d1.team), new Set ()));
+    const dataSums = summarizeData(incomingData, teams);
 
+    // Create the scales
     const yScale = d3.scaleBand()
       .domain(teams)
       .range([margin.top, svgHeight - margin.bottom])
       .padding(0.2)
       .round(true);
-
-    const yaxis = d3.axisLeft()
-      .scale(yScale)
-      .tickSize(tickSize)
-      .ticks(4);
-
-    d3.selectAll("svg")
-      .append("g")
-      .attr("id","yAxisG")
-      .call(yaxis)
-      .attr("transform","translate(" + margin.left + "," + margin.top + ")");
 
     const xScale_InterState = d3.scaleBand()
       .domain(["BL","IP", "Done"])
@@ -87,17 +65,31 @@ const colors = ["#C0392B", "#E59866", "#FCF3CF"];
       .padding(0.1)
       .round(true);
 
-    const labelMargin = 30;
-
     const xScale_State = d3.scaleLinear()
       .domain([ 0, 15 ])
       .range([ labelMargin, xScale_InterState.bandwidth() - labelMargin]);
+
+    // Define the axis functions
+    const yaxis = d3.axisLeft()
+      .scale(yScale)
+      .tickSize(tickSize)
+      .ticks(4);
 
     const xAxis = d3.axisTop()
       .scale(xScale_State)
       .tickSize(tickSize)
       .ticks(4);
 
+    // Start with globals for the graph.
+    d3.selectAll("svg")
+      .append("g")
+      .attr("id","yAxisG")
+      .call(yaxis)
+      .attr("transform","translate(" + margin.left + "," + margin.top + ")");
+
+    // TODO: Add legend
+
+    // Create the chart for each 'category'
     const charts = d3.select("svg")
       .selectAll("g.stackedBars")
       .data(dataSums)
@@ -111,21 +103,32 @@ const colors = ["#C0392B", "#E59866", "#FCF3CF"];
     charts.each(function(d,i){
       const chart = d3.select(this);
 
-      const xAxis = d3.axisTop()
-        .scale(xScale_State)
-        .tickSize(tickSize)
-        .ticks(4);
-
       chart
         .append("g")
         .call(xAxis)
         .attr("transform", "translate(" + xScale_InterState(d.state) + "," + svgHeight + ")");
 
-      const barRegions = chart.selectAll("g."+d.state)
+      chart.append("svg")
+        .attr("class", "axisLabel")
+        .attr("width", xScale_InterState.bandwidth())
+        .attr("height", labelHeight)
+        .attr("transform", "translate(" + (xScale_InterState(d.state)) + "," + svgHeight + ")")
+        .append("text")
+        .text(d => d.state)
+        .style("text-anchor", "middle")
+        .style("dominant-baseline", "central")
+        .style("fill", "black")
+        .style("font-size", "14px")
+        .attr("y", "50%")
+        .attr("x", "50%");
+
+      // Create a barRegion for each 'team'
+      const barRegions = chart.selectAll("g.state")
         .data(d.teams)
         .enter()
         .append("g")
         .attr("id", d => d.state)
+        .attr("class", "state")
         .attr("transform", d1 => "translate(" + xScale_InterState(d.state) + ", " + margin.top + ")");
 
       barRegions.each(function(team,i1){
@@ -137,6 +140,7 @@ const colors = ["#C0392B", "#E59866", "#FCF3CF"];
           .attr("y", yScale(team.key) + yScale.bandwidth() / 2)
           .attr("x", "0");
 
+        // Create a bar segment for each 'severity'
         const bars = teamBarRegion.selectAll("svg.bars")
           .data(team.values)
           .enter()
@@ -149,12 +153,11 @@ const colors = ["#C0392B", "#E59866", "#FCF3CF"];
           .attr("width", d1 => xScale_State(d1.value) - labelMargin);
 
         bars.append("rect")
-          .style("fill", (d1,i1) => colors[i1])
           .style("stroke", "black")
           .style("stroke-width", "1px")
           .attr("x","0")
           .attr("y","0")
-          .attr("class", "measurementRectangle")
+          .attr("class", d => "measurementRectangle " + d.key)
           .attr("height", d1 => yScale.bandwidth())
           .attr("width", d1 => xScale_State(d1.value) - labelMargin);
 
@@ -167,23 +170,19 @@ const colors = ["#C0392B", "#E59866", "#FCF3CF"];
           .attr("x", "50%");
 
       });
-
     })
 
     const mRects = d3.selectAll("rect.measurementRectangle");
     mRects.on("mouseover", function(d) {
       const bar = d3.select(this);
-
-      // hack
-      bar.attr("temp", bar.style("fill"));
       bar.style("fill","red");
-
     });
 
     mRects.on("mouseout", function(d) {
       const bar = d3.select(this);
-      // hack
-      bar.style("fill",bar.attr("temp"));
+
+      // Let the stylesheet take over
+      bar.style("fill", "")
     });
   }
 }
