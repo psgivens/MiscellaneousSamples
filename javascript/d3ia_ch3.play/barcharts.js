0757141dc259f6fe66504e560e18646c4923dabf
psgivens <psgivens11@yahoo.com>
2018-01-10 20:46:04 -0800

    Code beautification. Making data summarizing more generic.

--- javascript/d3ia_ch3.play/barcharts.js
+++ javascript/d3ia_ch3.play/barcharts.js
@@ -2,53 +2,56 @@ function createBarCharts() {
   d3.csv("defects.csv", function(data) {
     overallTeamViz(data);
   })
-  function summarizeData(data, teams) {
-    const teamSums = d3.nest()
-      .key(d => d.state)
-      .key(d => d.team)
-      .key(d => d.source)
-      .key(d => d.severity)
-      .rollup(d => d3.sum(d, d1 => d1.count))
-      .entries(data);
 
-    const mapSource = source => {
-        const severities = source.values;
+  function formatData(data, categories) {
+    const mapCat2 = cat2Item => {
+        const cat3Items = cat2Item.values;
         let offset = 0;
-        severities.forEach(item => {
+        cat3Items.forEach(item => {
+          item.cat3Key = item.key;
           item.x0 = offset;
           offset += item.value;
           item.x1 = offset;
         })
         return {
-          key:source.key,
-          values:severities,
-          total:severities.reduce((acc,item)=>acc+item.value,0)
+          cat2Key:cat2Item.key,
+          cat3Values:cat3Items,
+          total:cat3Items.reduce((acc,item)=>acc+item.value,0)
         }
       }
 
-    const mapTeam = team => {
+    const mapCat1 = cat1Item => {
+      const cat2Items = cat1Item.values.map(mapCat2);
       return {
-        key:team.key,
-        values:team.values.map(mapSource),
-        max:20, // TODO: Calculate max
-        total:5
+        cat1Key:cat1Item.key,
+        cat2Values:cat2Items,
+        max:cat2Items.reduce((acc,item)=>Math.max(acc,item.total))
       }
     }
 
-    const teamSumsR = teamSums
-      .map(stateItem => Object.create({
-          state:stateItem.key,
-          teams:stateItem.values.map(mapTeam)
+    const nestedData = d3.nest()
+      .key(d => d[categories[0]])
+      .key(d => d[categories[1]])
+      .key(d => d[categories[2]])
+      .key(d => d[categories[3]])
+      .rollup(d => d3.sum(d, d1 => d1.count))
+      .entries(data);
+
+    const formattedData = nestedData
+      .map(cat0Item => Object.create({
+          cat0Key:cat0Item.key,
+          cat1Values:cat0Item.values.map(mapCat1)
         }));
 
-    return teamSumsR;
+    return formattedData;
   }
 
   function overallTeamViz(incomingData) {
 
-    // Set up sizing constants
+    /* Configure the graph with sizes.
+    ************************/
     const svgHeight = 200;
-    const svgWidth = 900;
+    const svgWidth = 1200;
     const margin = {top: 20, right: 120, bottom: 50, left: 70};
     const width = svgWidth - margin.left - margin.right;
     const height = svgHeight - margin.top - margin.bottom;
@@ -58,20 +61,25 @@ function createBarCharts() {
     const labelMargin = 30;
     const labelHeight = 30;
 
-    // Munge the data
+    /* Get the categories
+    ************************/
+    const states = Array.from(incomingData.reduce((acc,d1) => acc.add(d1.state), new Set ()));
     const teams = Array.from(incomingData.reduce((acc,d1) => acc.add(d1.team), new Set ()));
     const severities  = Array.from(incomingData.reduce((acc,d1) => acc.add(d1.severity), new Set ()));
     const sources  = Array.from(incomingData.reduce((acc,d1) => acc.add(d1.source), new Set ()));
-    const dataSums = summarizeData(incomingData, teams);
+
+    /* Munge the data
+    ************************/
+    const dataSums = formatData(incomingData, ["state","team","source","severity"]);
 
     // Create the scales
     const yScale = d3.scaleBand()
       .domain(teams)
       .range([margin.top, svgHeight - margin.bottom])
-      .padding(0.2)
+      .padding(0.00)
       .round(true);
 
-    const createSourceScale = (teamName) => {
+    const createInnerYScale = (teamName) => {
       const teamTop = yScale(teamName);
       const teamBottom = teamTop + yScale.bandwidth();
 
@@ -79,14 +87,14 @@ function createBarCharts() {
       return d3.scaleBand()
         .domain(sources)
         .range([teamTop, teamBottom])
-        .padding(0.2)
+        .padding(0.1)
         .round(true);
     };
 
     const xScale_InterState = d3.scaleBand()
-      .domain(["Back Log","In Progress", "Done"])
+      .domain(states)
       .range([ margin.left, width + margin.left ])
-      .padding(0.1)
+      .padding(0.0)
       .round(true);
 
     const xScale_State = d3.scaleLinear()
@@ -134,7 +142,7 @@ function createBarCharts() {
       .enter()
       .append("g")
       .attr("class", "stackedBars")
-      .attr("id", d => "xAxis_" + d.state)
+      .attr("id", d => "xAxis_" + d.cat0Key)
       .attr("width", xScale_InterState.bandwidth());
 
     charts.each(function(d,i){
@@ -143,7 +151,7 @@ function createBarCharts() {
       const xa = chart
         .append("g")
         .call(xAxis)
-        .attr("transform", "translate(" + xScale_InterState(d.state) + "," + svgHeight + ")");
+        .attr("transform", "translate(" + xScale_InterState(d.cat0Key) + "," + svgHeight + ")");
 
       xa.selectAll("path.domain")
         .attr("opacity", "0.2");
@@ -155,10 +163,10 @@ function createBarCharts() {
         .attr("class", "axisLabel")
         .attr("width", xScale_InterState.bandwidth())
         .attr("height", labelHeight)
-        .attr("x", xScale_InterState(d.state) )
+        .attr("x", xScale_InterState(d.cat0Key) )
         .attr("y", (svgHeight + 10))
         .append("text")
-        .text(d => d.state)
+        .text(d => d.cat0Key)
         .style("text-anchor", "middle")
         .style("dominant-baseline", "central")
         .style("fill", "black")
@@ -168,24 +176,24 @@ function createBarCharts() {
 
       // Create a barRegion for each 'team'
       const barRegions = chart.selectAll("svg.state")
-        .data(d.teams)
+        .data(d.cat1Values)
         .enter()
         .append("svg")
-        .attr("id", d => d.state)
+        .attr("id", d => d.cat0Key)
         .attr("class", "state")
-        .attr("x", xScale_InterState(d.state))
+        .attr("x", xScale_InterState(d.cat0Key))
         .attr("y", margin.top);
 
       barRegions.each(function(team,i1){
         const teamBarRegion = d3.select(this);
-        const yScale_Source = createSourceScale(team.key);
+        const yScale_Source = createInnerYScale(team.cat1Key);
 
         teamBarRegion.selectAll("svg.source_total")
-          .data(team.values)
+          .data(team.cat2Values)
           .enter()
           .append("svg")
           .attr("class", "source_total")
-          .attr("y", d1 => yScale_Source(d1.key))
+          .attr("y", d1 => yScale_Source(d1.cat2Key))
           .attr("x", "0")
           .attr("width", labelMargin)
           .attr("height", yScale_Source.bandwidth())
@@ -196,17 +204,27 @@ function createBarCharts() {
           .style("dominant-baseline", "central")
           .style("font-size", "10px");
 
+        // const guideLines = teamBarRegion.selectAll("line.guideLines")
+        //   .data(team.values)
+        //   .enter()
+        //   .append("line")
+        //   .attr("x1", "0")
+        //   .attr("x2", "100")
+        //   .attr("y1", d1 => yScale_Source(d1.key))
+        //   .attr("y2", d1 => yScale_Source(d1.key))
+        //   .attr("stroke-width", 2)
+        //   .attr("stroke", "black");
+
+
         // Create a bar segment for each 'severity'
         const bars = teamBarRegion.selectAll("svg.bars")
-          .data(team.values)
+          .data(team.cat2Values)
           .enter()
           .append("svg")
           .attr("class", "bars")
           .attr("x", d1 => xScale_State(0))
-          .attr("y", d1 => yScale(team.key))
-          // .attr("class", "measurementRectangle")
+          .attr("y", d1 => yScale(team.cat1Key))
           .attr("height", d1 => yScale.bandwidth())
-          .attr("logs", d1 => console.log(xScale_State(d1.total)))
           .attr("width", d1 => xScale_State(d1.total) - labelMargin);
 
         bars.each(function(source,i2){
@@ -224,12 +242,12 @@ function createBarCharts() {
 
           // Create a bar segment for each 'severity'
           const sourceBars = teamBarRegion.selectAll("svg.source_bars")
-            .data(source.values)
+            .data(source.cat3Values)
             .enter()
             .append("svg")
             .attr("class", "source_bars")
             .attr("x", d1 => xScale_State(d1.x0))
-            .attr("y", d1 => yScale_Source(source.key))
+            .attr("y", d1 => yScale_Source(source.cat2Key))
             .attr("class", "measurementRectangle")
             .attr("height", d1 => yScale_Source.bandwidth())
             .attr("width", d1 => xScale_State(d1.value) - labelMargin);
@@ -239,7 +257,7 @@ function createBarCharts() {
             .style("stroke-width", "1px")
             .attr("x","0")
             .attr("y","0")
-            .attr("class", d => "measurementRectangle " + d.key)
+            .attr("class", d => "measurementRectangle " + d.cat3Key)
             .attr("height", d1 => yScale.bandwidth())
             .attr("width", d1 => xScale_State(d1.value) - labelMargin);
 
@@ -255,9 +273,7 @@ function createBarCharts() {
           });
 
         });
-    //   });
-    // })
-    //
+
     const mRects = d3.selectAll("rect.measurementRectangle");
     mRects.on("mouseover", function(d) {
       const bar = d3.select(this);
@@ -271,5 +287,5 @@ function createBarCharts() {
       bar.style("fill", "")
      });
   })
-}
+  }
 }
